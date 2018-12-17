13ba2209585cf6df2b3fceb75f6471b60ac7aad6
Kamil <kam.popczyk@gmail.com>
2018-12-17 12:04:27 +0100

    sensors data

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+import json
+import urllib.request
 
 app = Flask(__name__)
 
@@ -9,10 +11,26 @@ def hello_world():
     return 'AirQuality'
 
 
-@app.route('/<station>', methods=['GET'])
-def station_page():
-    url = 'http://api.gios.gov.pl/pjp-api/rest/station/sensors/'
+@app.route('/location/<station_id>')
+def station_page(station_id):
+    url = 'http://api.gios.gov.pl/pjp-api/rest/station/sensors/{}'
+    url_getData = 'http://api.gios.gov.pl/pjp-api/rest/data/getData/{}'
 
+    with urllib.request.urlopen(url.format(station_id)) as url:
+        sensors_data = json.loads(url.read().decode())
+
+    print(sensors_data[0]['param']['paramName'])
+    print(sensors_data[0]['id'])
+
+    air_data = {}
+
+    for sensor in sensors_data:
+        with urllib.request.urlopen(url_getData.format(sensor['id'])) as url:
+            param_data = json.loads(url.read().decode())
+        air_data[sensor['param']['paramCode']] = param_data['values']
+        # air_data = {sensor}
+
+    return jsonify(air_data)
 
 if __name__ == '__main__':
     app.run()
